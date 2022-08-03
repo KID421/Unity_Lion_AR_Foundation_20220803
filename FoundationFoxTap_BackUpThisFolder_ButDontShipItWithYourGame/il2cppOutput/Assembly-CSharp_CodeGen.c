10767502f9921413d8071656e611c99f89d78dbb
KID <50666207+KID421@users.noreply.github.com>
2022-08-03 16:30:21 +0800

    Update 平面辨識

---
+++ FoundationFoxTap_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,57 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void KID.TapToSpawnObject::Awake()
+extern void TapToSpawnObject_Awake_mF4E5D33845356F4C195D8133CE6060C416D2CE60 (void);
+// 0x00000002 System.Void KID.TapToSpawnObject::Update()
+extern void TapToSpawnObject_Update_m616BCD41DD51D8248389575BDE9B68757AE00C5F (void);
+// 0x00000003 System.Void KID.TapToSpawnObject::TapAndSpawn()
+extern void TapToSpawnObject_TapAndSpawn_m88701DCF1D0FCA7BBC9919FEB6A689B01F7C9520 (void);
+// 0x00000004 System.Void KID.TapToSpawnObject::.ctor()
+extern void TapToSpawnObject__ctor_m38F90109D7AFEA9656CB72A8AA8588860948AF00 (void);
+static Il2CppMethodPointer s_methodPointers[4] = 
+{
+	TapToSpawnObject_Awake_mF4E5D33845356F4C195D8133CE6060C416D2CE60,
+	TapToSpawnObject_Update_m616BCD41DD51D8248389575BDE9B68757AE00C5F,
+	TapToSpawnObject_TapAndSpawn_m88701DCF1D0FCA7BBC9919FEB6A689B01F7C9520,
+	TapToSpawnObject__ctor_m38F90109D7AFEA9656CB72A8AA8588860948AF00,
+};
+static const int32_t s_InvokerIndices[4] = 
+{
+	2086,
+	2086,
+	2086,
+	2086,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	4,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
